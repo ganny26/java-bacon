842ebdb6afd5a66cef330b494c0c8eb27d74d4ce
= <=>
2018-10-27 14:45:23 +0530

    updated app.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import Store from './store/Store';
 
 ReactDOM.render(
-    <Provider store={Store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+<Provider store={Store}>
+  <App />
+</Provider>, document.getElementById('root'));
 
